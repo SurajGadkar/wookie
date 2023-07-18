b41f31e95002ddcaf9c900607b3db286f19f37ec
Suraj Gadkar <surajgadkar14@gmail.com>
2023-07-18 19:29:23 +0530

    Initial setup COmplete

---
+++ wookie/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained">Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ wookie/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#007bff",
+    },
+    secondary: {
+      main: "#ff9800",
+    },
+  },
+});
+
+export default theme;
